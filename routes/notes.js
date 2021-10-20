2253da0e2d009414064adde0735264f724e40241
erikaosterbur <erikaosterbur@gmail.com>
2021-10-19 20:28:42 -0500

    assigned ids to each note

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 const fs = require("fs");
 let notes = require("../db/db.json");
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/', (req, res) => {
   res.json(notes);
 });
   
 router.post('/', (req, res) => {
-  if (req.body) {
+  if (req.body) { 
+    const id = uuidv4();
+    req.body["id"] = id
     notes.push(req.body);
     fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
       if (err) {
